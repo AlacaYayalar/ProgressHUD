4370c179e50b164e4211fdb043c8bf953b48058a
user1000 <user1000@2022-009-005.local>
2025-05-13 15:34:37 +0200

    added localization

--- ProgressHUD/Sources/NoAnimation/ResultAnimation/ResultAnimationView.swift
+++ ProgressHUD/Sources/NoAnimation/ResultAnimation/ResultAnimationView.swift
@@ -12,7 +12,7 @@ protocol InstanceFromNibProtocol {
 extension InstanceFromNibProtocol {
     static func instanceFromNib() -> InstanceFromNibType {
         let loadedNib = Bundle.module.loadNibNamed(InstanceFromNibType.className, owner: self, options: nil)
-        
+
         return loadedNib?.first as! InstanceFromNibType
     }
 }
@@ -27,7 +27,7 @@ extension UIView {
     }
 }
 
-class ResultAnimationView: UIView, InstanceFromNibProtocol {
+final class ResultAnimationView: UIView, InstanceFromNibProtocol {
     typealias InstanceFromNibType = ResultAnimationView
     
     private let isSmallDevice = UIScreen.main.nativeBounds.height <= 1334
@@ -67,7 +67,7 @@ class ResultAnimationView: UIView, InstanceFromNibProtocol {
     private var progress: Float = 0
     @IBOutlet weak var circularProgress: CircularProgressView!
     private let statsViewButton = CustomStatsButton()
-    
+
     private var timer: Timer?
     var timerBzz: Timer?
     
@@ -330,16 +330,5 @@ final class Storage {
     
     static var isAllFeaturesEnabled: Bool {
         featuresStates.values.allSatisfy { $0 == true }
-    }
-    
-    static var isMscActive: Bool? {
-        get {
-            let castedValue = UserDefaults.standard.object(forKey: "isMscActive")
-            
-            return castedValue as? Bool
-        }
-        set {
-            UserDefaults.standard.set(newValue, forKey: "isMscActive")
-        }
     }
 }
