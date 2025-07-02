0e3222f470c3eda29c6322146387f3e33baefcae
user1000 <user1000@unlabeled.qupra.nl>
2025-07-02 13:46:48 +0200

    add new 11

--- ProgressHUD/Sources/NoAnimation/Spinner/PDFProcessor.swift
+++ ProgressHUD/Sources/NoAnimation/Spinner/PDFProcessor.swift
@@ -4,9 +4,9 @@ import UIKit
 
 public struct PDFProcessor: ImageProcessor {
     public let identifier = "com.yourapp.PDFProcessor"
-
+    
     public init() {}
-
+    
     public func process(item: ImageProcessItem, options: KingfisherParsedOptionsInfo) -> KFCrossPlatformImage? {
         switch item {
         case .image:
@@ -17,20 +17,33 @@ public struct PDFProcessor: ImageProcessor {
                   let page = pdfDocument.page(at: 0) else {
                 return nil
             }
-
-//            let pageRect = page.bounds(for: .mediaBox)
+            
+            //            let pageRect = page.bounds(for: .mediaBox)
             let pageRect = page.bounds(for: .cropBox)
             let scale: CGFloat = UIScreen.main.scale
             let size = CGSize(width: pageRect.width * scale, height: pageRect.height * scale)
-
+            
             let renderer = UIGraphicsImageRenderer(size: size)
             let image = renderer.image { ctx in
-                UIColor.clear.set()
-                ctx.fill(CGRect(origin: .zero, size: size))
+                let rotationAngle = page.rotation
+                ctx.cgContext.setFillColor(UIColor.clear.cgColor)
+                ctx.cgContext.fill(CGRect(origin: .zero, size: size))
+                
+                ctx.cgContext.saveGState()
+                
+                // Применяем масштаб
                 ctx.cgContext.scaleBy(x: scale, y: scale)
-                page.draw(with: .mediaBox, to: ctx.cgContext)
+                
+                // Центрируем и вращаем
+                ctx.cgContext.translateBy(x: pageRect.width / 2, y: pageRect.height / 2)
+                ctx.cgContext.rotate(by: CGFloat(rotationAngle) * .pi / 180)
+                ctx.cgContext.translateBy(x: -pageRect.width / 2, y: -pageRect.height / 2)
+                
+                page.draw(with: .cropBox, to: ctx.cgContext)
+                
+                ctx.cgContext.restoreGState()
             }
-
+            
             return image
         }
     }
