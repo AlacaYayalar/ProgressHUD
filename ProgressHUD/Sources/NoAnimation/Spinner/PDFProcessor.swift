09ed4cde38969c87884f42591e491a9446174947
user1000 <user1000@unlabeled.qupra.nl>
2025-07-02 14:01:46 +0200

    add new 14

--- ProgressHUD/Sources/NoAnimation/Spinner/PDFProcessor.swift
+++ ProgressHUD/Sources/NoAnimation/Spinner/PDFProcessor.swift
@@ -18,7 +18,31 @@ struct PDFProcessor: ImageProcessor {
         // Set desired thumbnail size
         let targetSize = CGSize(width: 300, height: 300) // or imageView.bounds.size if dynamic
         let image = page.thumbnail(of: targetSize, for: .cropBox)
-        return image
+        
+        return image.rotate(radians: .pi)
     }
 }
 
+extension UIImage {
+    func rotate(radians: Float) -> UIImage? {
+        var newSize = CGRect(origin: CGPoint.zero, size: self.size).applying(CGAffineTransform(rotationAngle: CGFloat(radians))).size
+        // Trim off the extremely small float value to prevent core graphics from rounding it up
+        newSize.width = floor(newSize.width)
+        newSize.height = floor(newSize.height)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, self.scale)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        
+        // Move origin to middle
+        context.translateBy(x: newSize.width/2, y: newSize.height/2)
+        // Rotate around middle
+        context.rotate(by: CGFloat(radians))
+        // Draw the image at its center
+        self.draw(in: CGRect(x: -self.size.width/2, y: -self.size.height/2, width: self.size.width, height: self.size.height))
+    
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+}
