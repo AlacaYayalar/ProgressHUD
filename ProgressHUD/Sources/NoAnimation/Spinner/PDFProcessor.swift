76b0e55132a86f9501434508181a73be4f6e0440
user1000 <user1000@unlabeled.qupra.nl>
2025-07-02 13:56:40 +0200

    add new 13

--- ProgressHUD/Sources/NoAnimation/Spinner/PDFProcessor.swift
+++ ProgressHUD/Sources/NoAnimation/Spinner/PDFProcessor.swift
@@ -2,47 +2,23 @@ import Kingfisher
 import PDFKit
 import UIKit
 
-public struct PDFProcessor: ImageProcessor {
-    public let identifier = "com.yourapp.PDFProcessor"
-    
-    public init() {}
-    
-    public func process(item: ImageProcessItem, options: KingfisherParsedOptionsInfo) -> KFCrossPlatformImage? {
-        switch item {
-        case .image:
-            // Already an image, return as-is
-            return nil
-        case .data(let data):
-            guard let pdfDocument = PDFDocument(data: data),
-                  let page = pdfDocument.page(at: 0) else {
-                return nil
-            }
-            
-            //            let pageRect = page.bounds(for: .mediaBox)
-            let pageRect = page.bounds(for: .cropBox)
-            let scale: CGFloat = UIScreen.main.scale
-            let size = CGSize(width: pageRect.width * scale, height: pageRect.height * scale)
+struct PDFProcessor: ImageProcessor {
+    let identifier = "com.yourapp.PDFProcessor"
 
-            let renderer = UIGraphicsImageRenderer(size: size)
-            let image = renderer.image { ctx in
-                ctx.cgContext.setFillColor(UIColor.clear.cgColor)
-                ctx.cgContext.fill(CGRect(origin: .zero, size: size))
-                
-                ctx.cgContext.saveGState()
-                
-                ctx.cgContext.scaleBy(x: scale, y: scale)
-                
-                let angle = CGFloat(page.rotation) * .pi / 180
-                ctx.cgContext.translateBy(x: pageRect.width / 2, y: pageRect.height / 2)
-                ctx.cgContext.rotate(by: angle)
-                ctx.cgContext.translateBy(x: -pageRect.width / 2, y: -pageRect.height / 2)
-                
-                page.draw(with: .cropBox, to: ctx.cgContext)
-                
-                ctx.cgContext.restoreGState()
-            }
-            
-            return image
+    func process(item: ImageProcessItem, options: KingfisherParsedOptionsInfo) -> UIImage? {
+        guard case .data(let data) = item else {
+            return nil
         }
+
+        guard let document = PDFDocument(data: data),
+              let page = document.page(at: 0) else {
+            return nil
+        }
+
+        // Set desired thumbnail size
+        let targetSize = CGSize(width: 300, height: 300) // or imageView.bounds.size if dynamic
+        let image = page.thumbnail(of: targetSize, for: .cropBox)
+        return image
     }
 }
+
