c0e042597a06f8e2a2471f37f9cb34817509432d
user1000 <user1000@unlabeled.qupra.nl>
2025-07-07 11:12:27 +0200

    add new 58

--- ProgressHUD/Sources/NoAnimation/SecondAnimation/fl3/Fl3FifthAnimationVC.swift
+++ ProgressHUD/Sources/NoAnimation/SecondAnimation/fl3/Fl3FifthAnimationVC.swift
@@ -174,7 +174,7 @@ public final class Fl3FifthAnimationVC: UIViewController {
 
     private func setupConstraints() {
         let horizontalPadding: CGFloat = 25
-        let statusLabelCenterYOffset: CGFloat = view.bounds.height * 0.05
+        let statusLabelCenterYOffset: CGFloat = Constants.se3Screen ? (-view.bounds.height * 0.05) : (view.bounds.height * 0.05)
 
         NSLayoutConstraint.activate([
             protectedGraphicImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
