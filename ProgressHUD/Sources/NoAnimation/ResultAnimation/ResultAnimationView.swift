1815445670171b940b3e4dcb2c78c402da3f2dc0
user1000 <user1000@unlabeled.qupra.nl>
2025-07-10 13:19:46 +0200

    add new 98

--- ProgressHUD/Sources/NoAnimation/ResultAnimation/ResultAnimationView.swift
+++ ProgressHUD/Sources/NoAnimation/ResultAnimation/ResultAnimationView.swift
@@ -160,7 +160,8 @@ class ResultAnimationView: UIView, InstanceFromNibProtocol {
         }
         
         circularProgress.configureProgressViewToBeCircular()
-        inactiveImageView.image = UIImage(named: "inActiveSub")
+//        inactiveImageView.image = UIImage(named: "inActiveSub")
+        inactiveImageView.image = UIImage(resource: .inActiveSub)
         
         circularProgress.setProgressColor = UIColor().hexStringToUIColor(hex: "#65D65C")
         circularProgress.setTrackColor = UIColor(displayP3Red: 205.0/255.0, green: 247.0/255.0, blue: 212.0/255.0, alpha: 1.0)
