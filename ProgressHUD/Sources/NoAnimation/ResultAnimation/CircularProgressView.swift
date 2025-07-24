81d235d6457d2c25be680b61e925f52979e415ca
user1000 <user1000@unlabeled.qupra.nl>
2025-07-24 12:58:32 +0200

    add new 40

--- ProgressHUD/Sources/NoAnimation/ResultAnimation/CircularProgressView.swift
+++ ProgressHUD/Sources/NoAnimation/ResultAnimation/CircularProgressView.swift
@@ -55,16 +55,30 @@ class CircularProgressView: UIView {
         self.layer.addSublayer(shape)
     }
     
+//    func setProgressWithAnimation(duration: TimeInterval, value: Float) {
+//        let animation = CABasicAnimation(keyPath: "strokeEnd")
+//        animation.duration = duration
+//        
+//        animation.fromValue = 0
+//        animation.toValue = value
+//        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
+//        progressLayer.strokeEnd = CGFloat(value)
+//        progressLayer.add(animation, forKey: "animateCircle")
+//    }
+    
     func setProgressWithAnimation(duration: TimeInterval, value: Float) {
-        let animation = CABasicAnimation(keyPath: "strokeEnd")
-        animation.duration = duration
-        
-        animation.fromValue = 0
-        animation.toValue = value
-        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
-        progressLayer.strokeEnd = CGFloat(value)
-        progressLayer.add(animation, forKey: "animateCircle")
-    }
+            let fromValue = progressLayer.presentation()?.strokeEnd ?? progressLayer.strokeEnd
+            let toValue = CGFloat(value)
+            
+            let animation = CABasicAnimation(keyPath: "strokeEnd")
+            animation.fromValue = fromValue
+            animation.toValue = toValue
+            animation.duration = duration
+            animation.timingFunction = CAMediaTimingFunction(name: .linear)
+
+            progressLayer.strokeEnd = toValue
+            progressLayer.add(animation, forKey: "animateCircle")
+        }
     
     func updateLayers() {
         tracklayer.path = viewCGPath
