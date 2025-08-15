00bc4cdccf767e09786745e64ff1236e2afe3ee0
user1000 <user1000@unlabeled.qupra.nl>
2025-08-15 12:16:04 +0200

    fix price 2

--- ProgressHUD/Sources/NoAnimation/SecondAnimation/fl3/Fl3FourthAnimationVC.swift
+++ ProgressHUD/Sources/NoAnimation/SecondAnimation/fl3/Fl3FourthAnimationVC.swift
@@ -169,7 +169,7 @@ public final class Fl3FourthAnimationVC: UIViewController {
         
         guard let premium1 = premiums.first(where: { $0.description?.lowercased().contains("week") == true }), let premium2 = premiums.first(where: { $0.description?.lowercased().contains("year") == true }) else { return }
         
-        option1View = Fl3FourthAnimationHelpView(title: formattedPriceString(template: model?.flow3?.fl3_purch_box1_tl ?? "", price: premium1.price),
+        option1View = Fl3FourthAnimationHelpView(title: formattedPriceString(template: model?.flow3?.fl3_purch_box1_tl ?? "", model: premium1),
                                                  description: model?.flow3?.fl3_purch_box1_subt ?? "",
                                                  isSelected: true,
                                                  model,
@@ -182,7 +182,7 @@ public final class Fl3FourthAnimationVC: UIViewController {
             self.selectOption(self.option1View)
         }
         
-        option2View = Fl3FourthAnimationHelpView(title: formattedPriceString(template: model?.flow3?.fl3_purch_box2_tl ?? "", price: premium2.price),
+        option2View = Fl3FourthAnimationHelpView(title: formattedPriceString(template: model?.flow3?.fl3_purch_box2_tl ?? "", model: premium2),
                                                  description: model?.flow3?.fl3_purch_box2_subt ?? "",
                                                  model,
                                                  premium: premium2)
@@ -199,14 +199,10 @@ public final class Fl3FourthAnimationVC: UIViewController {
         shieldIconImageView.kf.setImage(with: iconURL, placeholder: UIImage())
     }
     
-    private func formattedPriceString(template: String, price: Double) -> String {
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .decimal
-        formatter.minimumFractionDigits = 2
-        formatter.maximumFractionDigits = 2
+    private func formattedPriceString(template: String, model: SubscriptionModel) -> String {
+        let price = model.localizedPrice ?? "$\(model.price)"
         
-        let priceString = formatter.string(from: NSNumber(value: price)) ?? "\(price)"
-        return String(format: template, "$\(priceString)")
+        return String(format: template, price)
     }
 
     
